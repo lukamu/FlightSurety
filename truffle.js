a21dcf4a0ea18a5787a1e39a298d3a78b5c00d7e
Luca <lukamusso@gmail.com>
2019-06-23 16:17:28 -0500

    fix: address argument was missing in deploy fuction

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "";
+var mnemonic = "vivid cabbage script team path coyote manual sunset chalk advance rebel foam";
 
 module.exports = {
   networks: {
     development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 3999999
+    },
+    development2: {
       provider: function() {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
